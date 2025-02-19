ab7e9c2e68afd1220fbc24cb733466f7456c9ff1
Yersaiyn <ersaiynabdull2005gmail.com>
2025-02-19 14:24:27 +0500

    hw20

--- templates/templatetags/custom_filters.py
+++ templates/templatetags/custom_filters.py
@@ -5,10 +5,8 @@ register = template.Library()
 
 @register.filter
 def filter_orders_by_status(orders, status):
-    """Фильтрует заказы по переданному статусу."""
     return orders.filter(status=status)
 
 @register.filter
 def filter_menu_by_category(menu_items, category):
-    """Фильтрует меню по переданной категории."""
     return menu_items.filter(category=category)
